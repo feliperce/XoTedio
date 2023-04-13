b0db8ade306bce806ae2014539d84e2bea466c2a
Felipe Rodrigues <felipe.rce@gmail.com>
2023-04-13 18:31:26 -0300

    gradle cleanup, date extension past fix

--- home/src/test/java/br/com/mobileti/commons/extensions/DateExtensionsKtTest.kt
+++ home/src/test/java/br/com/mobileti/commons/extensions/DateExtensionsKtTest.kt
@@ -8,7 +8,7 @@ internal class DateExtensionsKtTest {
 
     @Test
     fun `GIVEN date THEN get passed hours`() {
-        val hours24 = Date(1681401600000).getPassedHours(Date(1681315200000))
+        val hours24 = Date(1681135200000).getPassedHours(Date(1681221600000))
         val hours1 = Date(1681405200000).getPassedHours(Date(1681408800000))
         val hours12 = Date(1681239600000).getPassedHours(Date(1681282800000))
         val hours0 = Date(1681282800000).getPassedHours(Date(1681282800000))
@@ -17,5 +17,12 @@ internal class DateExtensionsKtTest {
         assert(hours1 == 1L)
         assert(hours12 == 12L)
         assert(hours0 == 0L)
+    }
+
+    @Test
+    fun `GIVEN wrong date THEN not get passed hours`() {
+        val wrongDate = Date(1681369200000).getPassedHours(Date(1681282800000))
+
+        assert(wrongDate == 0L)
     }
 }
